a27e333088de49e92877be375bc3be780da73e33
Sujin <sujinki6020@gmail.com>
2018-05-02 10:42:39 +0900

    sql sujin

---
+++ src/sql/mypage/assignment.sql
@@ -0,0 +1,42 @@
+create table tb_assignment (
+asgmt_no number(3) primary key,
+course_no number(3),
+student_no number(10),
+title varchar2(30),
+deadline date,
+reg_date date default sysdate,
+view_cnt number(3)
+);
+
+create table tb_assignment_submit (
+asgmt_submit_no number(3) primary key,
+student_no number(10),
+course_no number(3),
+file_path varchar2(100),
+sys_name varchar2(50),
+orig_name varchar2(50),
+submit_date date default sysdate,
+asgmt_grade number(3)
+);
+
+select * from tb_assignment;
+desc tb_assignment;
+select * from tb_assignment_submit;
+
+create sequence s_asgmt_no;
+
+create sequence s_asgmt_submit_no;
+
+select * from user_sequences;
+
+commit;
+
+alter table tb_assignment 
+modify view_cnt number(3) default 0;
+
+
+insert into tb_assignment(
+asgmt_no, course_no, student_no, title, deadline
+) values( 
+s_asgmt_no, 1, 1111111111, 'ENGLISH', DATE '2018-05-17'
+);
